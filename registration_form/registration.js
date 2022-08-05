295eb25bc0f95ea45e8b4290ef6d934ff422937a
ssabhijith2000 <abhijith.ss@inapp.com>
2022-08-05 15:42:36 +0530

    completed registration_form

--- registration_form/registration.js
+++ registration_form/registration.js
@@ -1,6 +1,21 @@
 
-findIfNotEmpty(input) {
-    if (input.trim().length != 0) return true;
-  }
 
-fullName = document.getElementById('')
+var submit = document.getElementById('submit')
+var inputs = Array.prototype.slice.call(document.querySelectorAll('.input'))
+
+function findIfNotEmpty(input) {
+  if (input.trim().length != 0) return true;
+}
+
+function validate(){
+  inputs.forEach(input => { 
+  inputvalue = input.querySelector('input').value
+  console.log(inputvalue)
+  if( findIfNotEmpty(inputvalue))
+  input.querySelector('p').style.visibility = "hidden"
+  else
+  input.querySelector('p').style.visibility = "visible"
+});
+}
+submit.addEventListener('click',validate)
+
