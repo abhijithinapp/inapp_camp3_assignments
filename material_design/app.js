f92b7de57e3cd5832991b49d90ee0eb2969642c7
abhijithinapp <109217423+abhijithinapp@users.noreply.github.com>
2022-08-12 01:01:23 +0530

    FINISHED

--- material_design/app.js
+++ material_design/app.js
@@ -5,10 +5,32 @@ import {MDCTopAppBar} from '@material/top-app-bar';
 import {MDCDataTable} from '@material/data-table';
 const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
 
+//floating action btn
+const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
 
-// Instantiation
+
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
+});
 const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
 iconButtonRipple.unbounded = true;
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+import {MDCMenu} from '@material/menu';
+
+const menu = new MDCMenu(document.querySelector('.mdc-menu'));
+menu.open = true;
+
+import {MDCList} from "@material/list";
+const list = MDCList.attachTo(document.querySelector('.mdc-list'));
+list.wrapFocus = true;
+
+import {MDCDrawer} from "@material/drawer";
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+import {MDCTabBar} from '@material/tab-bar';
+
+const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
